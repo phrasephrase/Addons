ace64cc739e73f65d6449d00d8ae656e2910af5c
Veniamin Phrase <veniamintereshchenko@mail.ru>
2024-10-01 11:00:52 +0300

    Added command /tpa, /tpaccept and /tpdeny

---
+++ src/main/java/phrase/addons/command/TpAccept.java
@@ -0,0 +1,44 @@
+package phrase.addons.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class TpAccept implements CommandExecutor {
+
+    private Tpa tpa = new Tpa();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
+                             @NotNull String s, @NotNull String[] strings) {
+
+        if(!(commandSender instanceof Player)) {
+            commandSender.sendMessage("0");
+        }
+
+        Player targetPlayer = (Player) commandSender;
+
+        UUID playerUUID = tpa.getPlayers().remove(targetPlayer.getUniqueId());
+        Player player = Bukkit.getPlayer(playerUUID);
+
+        if(player == null) {
+            System.out.println("1");
+        }
+
+        targetPlayer.sendMessage("2");
+        player.teleport(targetPlayer.getLocation());
+        player.sendMessage("3");
+
+        return true;
+    }
+
+    public String color(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+}
---
+++ src/main/java/phrase/addons/command/Tpa.java
@@ -0,0 +1,52 @@
+package phrase.addons.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Tpa implements CommandExecutor {
+
+    private HashMap<UUID, UUID> players = new HashMap();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
+                             @NotNull String s, @NotNull String[] strings) {
+
+        if(!(commandSender instanceof Player)) {
+            commandSender.sendMessage("0");
+        }
+
+        Player player = (Player) commandSender;
+
+        if(strings.length < 1) {
+            commandSender.sendMessage("1");
+        }
+
+        Player targetPlayer = Bukkit.getPlayer(strings[1]);
+
+        if(targetPlayer.isOnline() || targetPlayer == null) {
+            commandSender.sendMessage("2");
+        }
+
+        players.put(targetPlayer.getUniqueId(), player.getUniqueId());
+        commandSender.sendMessage("3");
+        targetPlayer.sendMessage("4");
+
+        return true;
+    }
+
+    public HashMap<UUID, UUID> getPlayers() {
+        return players;
+    }
+
+    public String color(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+}
